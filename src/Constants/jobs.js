9114863a5cfc16db14e3a9c2908f42ed1950640b
McKenzie <mckenziechurch19@gmail.com>
2023-01-09 15:35:47 -0500

    deleted particle.js lib

---
+++ src/Constants/jobs.js
@@ -0,0 +1,8 @@
+const JOBS = [
+    {
+        position: "Software Engineering Intern",
+        start_date: Date.UTC(2022, 5),
+        end_date: "Present",
+        company: "The Home Depot"
+    }
+]
